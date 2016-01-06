f2586350fda17a569041c43d6a0f31d18c94f487
Mark Daniels <github.markdaniels@gmail.com>
2016-01-05 16:28:48 -0800

    removed the forward slash from href attribute

--- dashboard/application/views/partials/navbar.php
+++ dashboard/application/views/partials/navbar.php
@@ -1,14 +1,14 @@
 <div class="navbar navbar-default">
 	<div class="container">
 		<div class="navbar-header">
-			<a class="navbar-brand" href="/">Test App</a>
+			<a class="navbar-brand" href="users">Test App</a>
 		</div>
 		<div>
 			<ul class="nav navbar-nav">
-				<li class="active"><a href="/">Home</a></li>
+				<li class="active"><a href="users">Home</a></li>
 			</ul>
 			<ul class="nav navbar-nav navbar-right">
-				<li><a href="/users/signin">Sign in</a></li>
+				<li><a href="signin">Sign in</a></li>
 			</ul>
 		</div>
 	</div>
