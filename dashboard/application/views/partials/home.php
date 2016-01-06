9c9f0d9db545fc0ddf99d4a5620689a996915223
Mark Daniels <github.markdaniels@gmail.com>
2016-01-05 16:28:12 -0800

    removed the forward slash from href attribute

--- dashboard/application/views/partials/home.php
+++ dashboard/application/views/partials/home.php
@@ -14,17 +14,17 @@
 	<div class="col-md-4">
 		<h2>Manage Users</h2>
 		<p>Using this application, you'll learn how to add, remove, and edit users for the application.</p>
-		<p><a class="btn btn-default" href="/dashboard/admin">Manage Users &raquo;</a></p>
+		<p><a class="btn btn-default" href="dashboard/admin">Manage Users &raquo;</a></p>
 	</div>
 	<div class="col-md-4">
 		<h2>Leave Messages</h2>
 		<p>Users will be able to leave a message to another user using the same application.</p>
-		<p><a class="btn btn-default" href="/dashboard">Leave a message &raquo;</a></p>
+		<p><a class="btn btn-default" href="dashboard">Leave a message &raquo;</a></p>
 	</div>
 	<div class="col-md-4">
 		<h2>Edit User Information</h2>
 		<p>Admins will be able to edit another user's information (email address, first name, last name, etc).</p>
-		<p><a class="btn btn-default" href="/dashboard/admin">Edit user &raquo;</a></p>
+		<p><a class="btn btn-default" href="dashboard/admin">Edit user &raquo;</a></p>
 	</div>
 </div>
 <hr>
