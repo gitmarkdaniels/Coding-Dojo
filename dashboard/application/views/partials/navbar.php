0f315428df6da084af59d64a338a9ee58ff516b9
Mark Daniels <github.markdaniels@gmail.com>
2016-01-06 16:36:49 -0800

    added /dashboard/ to href anchor attribute

--- dashboard/application/views/partials/navbar.php
+++ dashboard/application/views/partials/navbar.php
@@ -5,10 +5,10 @@
 		</div>
 		<div>
 			<ul class="nav navbar-nav">
-				<li class="active"><a href="users">Home</a></li>
+				<li class="active"><a href="/dashboard/users">Home</a></li>
 			</ul>
 			<ul class="nav navbar-nav navbar-right">
-				<li><a href="signin">Sign in</a></li>
+				<li><a href="/dashboard/signin">Sign in</a></li>
 			</ul>
 		</div>
 	</div>
